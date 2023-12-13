d54cd411914a804b41b91764c8d9164e1fb11c1d
Bryan Burke <bryan.t.burke@gmail.com>
2023-12-13 16:24:33 -0500

    build def for multi-page site

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,20 @@ import { resolve } from 'path';
 import handlebars from 'vite-plugin-handlebars';
 
 export default {
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        benefits: resolve(__dirname, 'benefits.html'),
+        federal: resolve(__dirname, 'federal.html'),
+        faq: resolve(__dirname, 'faq.html'),
+        other: resolve(__dirname, 'other-opportunities.html'),
+        positions: resolve(__dirname, 'positions.html'),
+        process: resolve(__dirname, 'process.html'),
+        jpo: resolve(__dirname, 'students-jpo.html'),
+      },
+    },
+  },
   plugins: [
     handlebars({
       partialDirectory: resolve(__dirname, 'partials'),
