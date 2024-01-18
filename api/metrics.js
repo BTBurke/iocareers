31243510e778f31e4cf32184c24d9c6cdb1b0c61
Bryan Burke <bryan.t.burke@gmail.com>
2024-01-18 14:17:43 +0000

    update metrics endpoint with diagnostic logging

--- api/metrics.js
+++ api/metrics.js
@@ -1,4 +1,4 @@
 export default function (request, response) {
-  console.log('metric OK', request.path)
+  console.log('metric OK', JSON.parse(request.body))
   return response.status(200).send('OK')
 }
